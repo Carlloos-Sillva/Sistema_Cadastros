ae8fa8a532a080f93a9776a963f9630a72a493f8
Carlos Eduardo da Silva <80638432+Carlloos-Sillva@users.noreply.github.com>
2021-09-09 18:34:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<title>Hardware & Software</title>
+
+		<!-- jquery - link cdn -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+		<!-- bootstrap - link cdn -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	
+		<script>
+			// código javascript						
+		</script>
+	</head>
+
+	<body>
+
+		<!-- Static navbar -->
+	    <nav class="navbar navbar-default navbar-static-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <img src="imagens/logo.png" />
+	        </div>
+	        
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="inscrevase.php">Inscrever-se</a></li>
+	            <li class="">
+	            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
+					<ul class="dropdown-menu" aria-labelledby="entrar">
+						<div class="col-md-12">
+				    		<p>Você possui uma conta?</h3>
+				    		<br />
+							
+								<br /><br />
+								
+							</form>
+						</form>
+				  	</ul>
+	            </li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+	    <br>
+
+
+	    <div class="container">
+	    	<h1><strong>Hardware & Software INFORMÁTICA</strong></h1>
+	    	<hr>
+
+						<div class="container">
+							<div class="row">
+							    <div class="col-sm-3">
+							     <img src="imagens/logo_tec.png" alt="..." class="rounded-circle" max width="70%">
+							    </div>
+							    <div class="col-sm-9">
+							     
+							    <h2>Técnico Carlos E. Silvao</h2>	
+							     <p>
+								Sou Profissional na área de Tecnologia e também apaixonado por jogos e cinema. <br>
+
+								Conteudos sobre artigos de Hardware e Software.<br>
+
+								Bem vindo ao site sobre os conteudos de Hardware e Software corresponde aos componentes físicos do computador e desenvolvimento de sistemas ou seja<br>
+
+								são as peças e aparatos eletrônicos e programas para utilizar no seu dia a dia de trabalho e sua vida social e etc.<br>
+
+								O software é a parte referente aos sistemas que executam as atividades ou seja, são os programas e aplicativos que fazem com a máquina funcione cada vez melhor.<br>
+
+								Hardware e Software e o Universo da Informática é formado por: equipamentos, sistemas, dados e pessoas, esse conteudos explicará os conceitos de hardware e software.</p>
+							</div>
+						 </div>
+
+	    </div>
+
+	    <div class="container">
+	    	<hr>
+	    	<footer>
+	    		   		
+
+	    		<blockquote class="blockquote text-center">
+  			<h5><strong>Desenvolvido: Técnico Carlos E. Silva <br> WHATSAPP (**) 9****-****</strong></h5>
+				</blockquote>
+
+	    	</footer>
+	    </div>
+	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
