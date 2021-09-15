ea23a0891c645809efa8dd08c136f423ad3c2ac4
Carlos Eduardo da Silva <80638432+Carlloos-Sillva@users.noreply.github.com>
2021-09-15 18:45:24 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Hardware & Software</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div class="topo">
+		<img src="img/Inicial.png" alt="topo">		
+	</div>
+	<br><br><br>
+	<h1>Mundo de Hardware & Software</h1><hr><br>
+	<div class="pesquisa">
+		<h5>Técnico: Carlos E. Silva</h5>		
+	</div>
+	<div class="conteudo">
+		<?php
+			
+		?>
+		<br><br>
+		<a href="index.html"> << Voltar para pagina principal</a>
+	</div>
+	<div class="rodape">
+		<h2>Conteudos sobre Hardware e Software !!!</h2><br>
+	</div>
+	<footer>
+		<h6>Todos os direitos e reservados - Técnico Carlos E. Silva</h6>
+	</footer>
+</body>
+</html>
